ab6dc728106fbfd8667ca3f273c60a8f1d47521a
Robert Dodson <rad@robdodson.net>
2025-03-11 17:08:54 -0700

    add state

--- net.robdodson.robweather/plugin.js
+++ net.robdodson.robweather/plugin.js
@@ -12,7 +12,7 @@ function load()
 		//let uri = site + `?value=${json.value}&timestamp=${json.timestamp}`;
 		let item = Item.createWithUriDate(uri, date);
 		let temp=  Math.trunc(json.current.temp);
-		item.body = `<p>${city}</p>`;
+		item.body = `<p>${city},${state}</p>`;
 		item.body = item.body + `<p><b>Now:</b> ${temp}f ${json.current.weather[0].description}</p>`;
 		item.body = item.body + `<p><b>Today:</b> ${json.daily[0].summary}</p>`;
 		item.body = item.body + `<p><b>Tomorrow:</b> ${json.daily[1].summary}</p>`;
